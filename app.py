485795addc28b202c7acb3a748f2810516ba113b
auraCodesKM <kavinpadhle@gmail.com>
2025-01-31 15:23:29 +0530

    new

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  
 
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
 @app.route('/run-ollama', methods=['POST'])
 def run_ollama():
     data = request.get_json()  
